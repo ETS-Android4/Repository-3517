1bd11102469f859b6bd67ed501d39c21aa95999e
Eric-OI <zbi@webb.org>
2021-09-06 16:20:41 -0700

    Eric Bi
    commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Eric.java
@@ -0,0 +1,38 @@
+// My name is Eric (Eric.java)
+// 2021 - 2022 USACO rp ++ !!!
+/*code is far away from bug with the animal protecting
+ *  ┏┓　　　┏┓
+ *┏┛┻━━━┛┻┓
+ *┃　　　　　　　┃
+ *┃　　　━　　　┃
+ *┃　┳┛　┗┳　┃
+ *┃　　　　　　　┃
+ *┃　　　┻　　　┃
+ *┃　　　　　　　┃
+ *┗━┓　　　┏━┛
+ *　　┃　　　┃ 2021 - 2022 USACO rp++;
+ *　　┃　　　┃ 1c1a no bugs in code!!!
+ *　　┃　　　┗━━━┓
+ *　　┃　　　　　　　┣┓
+ *　　┃　　　　　　　┏┛
+ *　　┗┓┓┏━┳┓┏┛
+ *　　　┃┫┫　┃┫┫
+ *　　　┗┻┛　┗┻┛
+ *
+ */
+/*
+class Eric
+{
+    public static void main(String[] args)
+    {
+        String user = "admin";
+        double money = 10000;
+        String pwd = "123";
+        Scanner sc = new Scanner(System.in);
+        boolean b = login(user, pwd);
+        System.out.println(user + " " + pwd);
+    }
+}
+
+
+ */
