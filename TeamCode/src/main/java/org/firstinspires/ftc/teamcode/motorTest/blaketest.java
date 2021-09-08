67b10554b198ad04360cbe05fd5975e354546ae5
SwordMaster3341 <64105142+SwordMaster3341@users.noreply.github.com>
2021-09-08 16:07:46 -0700

    Fixed my code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest/blaketest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest/blaketest.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-import  pcom.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -13,8 +13,8 @@ public class TeleOp359 extends LinearOpMode {
     private DcMotor motorRight;
 
     public void runOpMode() throws InterruptedException {
-        motorLeft = hardewareMap.dcMotor.get("motorLeft")
-        motorRight = hardewareMap.dcMotor.get("motorRight")
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
 
         waitForStart();
         while (opModeIsActive()) {
@@ -25,8 +25,8 @@ public class TeleOp359 extends LinearOpMode {
             /**
              * Wheels
              */
-           motorLeft.setPower (-gamepad1.left_stick_y)
-            motorRight.setPower (-gamepad1.right_stick_y)
+           motorLeft.setPower (-gamepad1.left_stick_y);
+            motorRight.setPower (-gamepad1.right_stick_y);
         }
     }
 }
