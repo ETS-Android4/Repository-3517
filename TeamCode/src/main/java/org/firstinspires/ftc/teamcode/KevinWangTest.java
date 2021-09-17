9e365222ba668ef18a2dd8187702eb93694341c7
Kevin Wang <kevinwangqi2@gmail.com>
2021-09-17 14:49:15 -0700

    Update KevinWangTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KevinWangTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KevinWangTest.java
@@ -8,10 +8,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class KevinWangTest extends LinearOpMode {
 
     private DcMotor motor1;
+    private DcMotor motor2;
 
     public void runOpMode() throws InterruptedException {
 
         motor1 = hardwareMap.dcMotor.get("motor1");
+        motor2 = hardwareMap.dcMotor.get("motor2");
 
         waitForStart();
 
@@ -21,8 +23,9 @@ public class KevinWangTest extends LinearOpMode {
             telemetry.update();
 
             motor1.setPower(1);
+            motor2.setPower(1);
 
-            telemetry.addData("motor1's power is set to 1", "running");
+            telemetry.addData("motor1's power is set to 1, motor2's power is set to 1", "running");
             telemetry.update();
         }
 
