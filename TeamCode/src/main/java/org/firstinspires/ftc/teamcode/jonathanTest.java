83c707edef6ecdddf67416160c0a138d71e264db
JonathanLi13090 <65463483+JonathanLi13090@users.noreply.github.com>
2021-09-17 14:51:05 -0700

    Update jonathanTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jonathanTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jonathanTest.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import java.util.List;
 
-@TeleOp
+@Autonomous
 public class jonathanTest extends LinearOpMode {
 
     private DcMotor motorLeft;
