0cdc7711e3cbd63e5c94b168056468ed222fa2b5
JonathanLi13090 <65463483+JonathanLi13090@users.noreply.github.com>
2021-10-06 15:59:25 -0700

    first op mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jonathanTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jonathanTest.java
@@ -1,21 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import java.util.List;
 
-@Autonomous
+@TeleOp
 public class jonathanTest extends LinearOpMode {
 
     private DcMotor motorLeft;
     private DcMotor motorRight;
+    private DcMotor motorMiddle;
 
     public void runOpMode() throws InterruptedException {
 
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
+        motorMiddle = hardwareMap.dcMotor.get("motorMiddle");
 
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
 
@@ -25,14 +27,22 @@ public class jonathanTest extends LinearOpMode {
             telemetry.addData("opModeIsActive", opModeIsActive());
             telemetry.update();
 
-            motorLeft.setPower(1);
-            motorRight.setPower(1);
+            //motorLeft.setPower(0.5f);
+            //motorRight.setPower(0.5f);
 
             telemetry.addData("Motors Running", "Cool");
             telemetry.update();
+            if (Math.abs(gamepad1.left_stick_y) > 0){
+                motorLeft.setPower(-gamepad1.left_stick_y);
+                motorRight.setPower(-gamepad1.left_stick_y);
+            }
+            else if (Math.abs(gamepad1.right_stick_x) > 0){
+                motorLeft.setPower(gamepad1.right_stick_x);
+                motorRight.setPower(-gamepad1.right_stick_x);
+            }
+            motorMiddle.setPower(-gamepad1.left_stick_x);
 
-            //motorLeft.setPower(-gamepad1.left_stick_y);
-            //motorRight.setPower(-gamepad1.right_stick_y);
+
             //idle();
         }
     }
