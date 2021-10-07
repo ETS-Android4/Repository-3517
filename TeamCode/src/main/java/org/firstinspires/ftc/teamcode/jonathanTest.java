26f39b05b7fcf4f9906eac0faf3839a91ec09b12
JonathanLi13090 <65463483+JonathanLi13090@users.noreply.github.com>
2021-10-06 19:04:47 -0700

    fix 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jonathanTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jonathanTest.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import java.util.List;
 
-@TeleOp
+@TeleOp(name = "jonathanTest")
 public class jonathanTest extends LinearOpMode {
 
     private DcMotor motorLeft;
@@ -19,30 +19,27 @@ public class jonathanTest extends LinearOpMode {
         motorRight = hardwareMap.dcMotor.get("motorRight");
         motorMiddle = hardwareMap.dcMotor.get("motorMiddle");
 
-        motorLeft.setDirection(DcMotor.Direction.REVERSE);
-
         waitForStart();
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
 
         while (opModeIsActive()) {
             telemetry.addData("opModeIsActive", opModeIsActive());
             telemetry.update();
 
-            //motorLeft.setPower(0.5f);
-            //motorRight.setPower(0.5f);
-
             telemetry.addData("Motors Running", "Cool");
             telemetry.update();
-            if (gamepad1.left_stick_y > 0.8)
-            {
+            if (Math.abs(gamepad1.left_stick_y) > 0){
                 motorLeft.setPower(-gamepad1.left_stick_y);
                 motorRight.setPower(-gamepad1.left_stick_y);
             }
-            else if (gamepad1.left_stick_x > 0.8)
-            {
-                motorMiddle.setPower(-gamepad1.left_stick_x);
+            else if (Math.abs(gamepad1.right_stick_x) > 0){
+                motorLeft.setPower(gamepad1.right_stick_x);
+                motorRight.setPower(-gamepad1.right_stick_x);
             }
-            motorLeft.setPower(gamepad1.right_stick_x);
-            motorRight.setPower(-gamepad1.right_stick_x);
+            motorMiddle.setPower(-gamepad1.left_stick_x);
+
+
             //idle();
         }
     }
